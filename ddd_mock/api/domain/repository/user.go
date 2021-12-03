b75cb775755243c8940215b5338a6e2e17568549
sgash708 <sugash7081023@gmail.com>
2021-12-03 18:01:46 +0900

    repositoryクラスのinterfaceはinfra層で使うもの

--- ddd_mock/api/domain/repository/user.go
+++ ddd_mock/api/domain/repository/user.go
@@ -4,5 +4,6 @@ import "github.com/sgash708/go_test_design_mock/ddd_mock/api/domain/model"
 
 // User Userモデルへの登録できる構造体
 type User interface {
+	/* Infra層で使用する関数を記述する */
 	Create(u *model.User) (*model.User, error)
 }

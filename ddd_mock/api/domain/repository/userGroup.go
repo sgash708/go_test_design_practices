2e5a2ea4a714f548e9ba07c67e56ca4e4eabc7e5
sgash708 <sugash7081023@gmail.com>
2021-12-03 16:29:09 +0900

    repositoryを分割

--- ddd_mock/api/domain/repository/userGroup.go
+++ ddd_mock/api/domain/repository/userGroup.go
@@ -3,6 +3,6 @@ package repository
 import "github.com/sgash708/go_test_design_mock/ddd_mock/api/domain/model"
 
 // User Userモデルへの登録できる構造体
-type User interface {
-	Create(u *model.User) (*model.User, error)
+type UserGroup interface {
+	Create(usergroup *model.UserGroup) (*model.User, error)
 }
